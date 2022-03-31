1ab4e4b5b49eef6c85a2cc7054adc34d73227e58
Zed0x01 <sherifashrafmedia@gmail.com>
2022-03-31 03:40:08 +0200

    V2

--- FetchAPI/main.js
+++ FetchAPI/main.js
@@ -1,15 +1,34 @@
-let inputText = document.querySelector('.inputText');
-let button = document.querySelector('.btn1');
+let inputText = document.querySelector(".inputText");
+let button = document.querySelector(".btn1");
+let contentView = document.querySelector(".contentView");
 
-button.onclick = async () =>{
-    const respone = await fetch(inputText.value);
-    const jsonData = await respone.json();
-    jsonData.forEach(users => {
-        console.log(`
-        Name : ${users.name}
-        Username : ${users.username}
-        Phone : ${users.phone}
-        Website : ${users.website}
-        `);
+button.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const respone = await fetch(inputText.value, {
+        method: "GET",
+        accept: "application/text",
     });
+    const jsonData = await respone.text();
+    getElementsReq(jsonData);
+});
+
+function getElementsReq(data) {
+    let parseData = new DOMParser();
+    let parsedData = parseData.parseFromString(data, "text/html");
+    let pageTitle = parsedData.title;
+    let meta = parsedData.getElementsByTagName("meta");
+    
+    for (let i = 0; i < meta.length; i++) {
+        if (meta[i].getAttribute("name") && meta[i].getAttribute("content")) {
+            let firsth2 = document.createElement("h2");
+            firsth2.setAttribute("id", "titlePage");
+            firsth2.innerText = `Name : ${pageTitle}`;
+            contentView.append(firsth2);
+
+            let secondh2 = document.createElement("h2");
+            secondh2.setAttribute("id", "descriptionPage");
+            secondh2.innerText = `${meta[i].getAttribute("name")} : ${meta[i].getAttribute("content")}`;
+            contentView.append(secondh2);
+        }
+    }
 }
