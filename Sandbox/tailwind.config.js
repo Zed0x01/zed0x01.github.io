d7f9f8d441966795cca51817756990a7be02f4b9
Zed0x01 <sherifashrafmedia@gmail.com>
2022-04-16 04:40:22 +0200

    v1
    Still Need Responsive

---
+++ Sandbox/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      width:{
+        '1200' : '1170px',
+        '992' : '970px',
+        '768' : '750px'
+      },
+      gridTemplateColumns:{
+        '350':'repeat(auto-fill,minmax(350px,1fr))',
+        '300':'repeat(auto-fill,minmax(300px,1fr))',
+        '400':'repeat(auto-fill,minmax(400px,1fr))'
+      }
+    },
+  },
+  plugins: [],
+}
